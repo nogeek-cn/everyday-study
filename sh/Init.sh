34e70f125d87b136695e1291b08f510f638c484a
Darian <1934849492@qq.com>
2021-03-21 20:58:21 +0800

    ${commit_msg}

---
+++ sh/Init.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+git init
+git remote add gitee git@gitee.com:Darian1996/everyday-study.git
+git remote add github git@github.com:Darian1996/everyday-study.git
+git pull gitee master
+git pull github master
